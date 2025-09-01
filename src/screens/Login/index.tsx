96685001f692d57a77b8d28efa22815a0729ebad
Ricardo Baldrez <rdbz@gft.com>
2025-09-01 17:18:18 -0300

    feat: Add reach-hook-form package and init login form

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,20 +1,13 @@
-import { PublicStackParamsList } from "@/routes/PublicRoutes";
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { Text, TouchableOpacity, TextInput } from "react-native";
+import { View } from "react-native";
 import { DismissKeyboardView } from "@/components/DismissKeyboardView";
+import { LoginForm } from "./LoginForm";
 
 export const Login = () => {
-  const navigation =
-    useNavigation<StackNavigationProp<PublicStackParamsList>>();
-
   return (
     <DismissKeyboardView>
-      <Text>Tela de login!</Text>
-      <TextInput className="bg-gray-500 w-full" />
-      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-        <Text>Registrar</Text>
-      </TouchableOpacity>
+      <View className="flex-1 w-[82%] self-center">
+        <LoginForm />
+      </View>
     </DismissKeyboardView>
   );
 };
