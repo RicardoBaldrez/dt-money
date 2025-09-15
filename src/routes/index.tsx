62d0343bcd134466377e9991a8ebff206adf1afc
Ricardo Baldrez <rdbz@gft.com>
2025-09-15 18:31:27 -0300

    feat(Auth): Consume user data from global state

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,20 @@
+import { useCallback } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { PublicRoutes } from "@/routes/PublicRoutes";
-import { PrivateRoutes } from "@/routes/PrivateRoutes";
-import { useCallback, useState } from "react";
 import { SystemBars} from 'react-native-edge-to-edge'
 
+import { PublicRoutes } from "@/routes/PublicRoutes";
+import { PrivateRoutes } from "@/routes/PrivateRoutes";
+import { useAuthContext } from "@/context/auth.context";
+
 const NavigationRoutes = () => {
-  const [user, setUser] = useState(undefined);
+  const { user, token } = useAuthContext();
 
   const Routes = useCallback(() => {
-    if (!user) {
+    if (!user || !token) {
       return <PublicRoutes />;
     }
     return <PrivateRoutes />;
-  }, [user]);
+  }, [user, token]);
 
   return (
     <NavigationContainer>
