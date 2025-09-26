b448bd3f86f4a4f2082732bd28745a4bef31260c
Ricardo Baldrez <rdbz@gft.com>
2025-09-26 20:06:47 -0300

    feat: Add snackbar context

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import "./src/styles/global.css";
 import NavigationRoutes from "./src/routes";
 import { AuthContextProvider } from "@/context/auth.context";
+import { SnackbarContextProvider } from "@/context/snackbar.context";
 
 export default function App() {
   return (
-    <AuthContextProvider>
-      <NavigationRoutes />
-    </AuthContextProvider>
+    <SnackbarContextProvider>
+      <AuthContextProvider>
+        <NavigationRoutes />
+      </AuthContextProvider>
+    </SnackbarContextProvider>
   );
 }
