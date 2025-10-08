e9a7d683710345ea110891c474f60c0076230f54
Ricardo Baldrez <rdbz@gft.com>
2025-10-08 18:24:52 -0300

    feat: add validation to new transaction form

--- src/components/NewTransaction/index.tsx
+++ src/components/NewTransaction/index.tsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 import { View, TouchableOpacity, Text, TextInput } from "react-native";
 import CurrencyInput from "react-native-currency-input";
 import { MaterialIcons } from "@expo/vector-icons";
+import * as Yup from "yup";
 
 import { colors } from "@/shared/colors";
 import { ICreateTransactionRequest } from "@/shared/interfaces/https/createTransactionRequest";
 import { useBottomSheetContext } from "@/context";
 import { TransactionTypeSelector } from "../SelectType";
 import { SelectCategoryModal } from "../SelectCategoryModal";
+import { transactionSchema } from "./schema";
+import { AppButton } from "../AppButton";
+
+type ValidationErrorsTypes = Record<keyof ICreateTransactionRequest, string>;
 
 export const NewTransaction = () => {
   const { closeBottomSheet } = useBottomSheetContext();
+  const [validationErrors, setValidationErrors] =
+    useState<ValidationErrorsTypes>();
 
   const [transaction, setTransaction] = useState<ICreateTransactionRequest>({
     description: "",
@@ -18,6 +25,26 @@ export const NewTransaction = () => {
     categoryId: 0,
     value: 0,
   });
+
+  const handleCreatetransaction = async () => {
+    try {
+      await transactionSchema.validate(transaction, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const errors = {} as ValidationErrorsTypes;
+
+        error.inner.forEach((err) => {
+          if (err.path) {
+            errors[err.path as keyof ICreateTransactionRequest] = err.message;
+          }
+        });
+
+        setValidationErrors(errors);
+      }
+    }
+  };
 
   const setTransactionData = (
     key: keyof ICreateTransactionRequest,
@@ -63,6 +90,9 @@ export const NewTransaction = () => {
           typeId={transaction.typeId}
           setTransactionType={(typeId) => setTransactionData("typeId", typeId)}
         />
+        <View className="my-4">
+          <AppButton onPress={handleCreatetransaction}>Registrar</AppButton>
+        </View>
       </View>
     </View>
   );
